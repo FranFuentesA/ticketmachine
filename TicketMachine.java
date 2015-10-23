b51f5407482277edbfb35f1d0712abade93dd2fd
Francisco Fuentes <fran_fuentes20@hotmail.com>
2015-10-23 13:12:42 +0200

    he cmbiado los parametros del constructor para que el billete siempre valga 800

--- TicketMachine.java
+++ TicketMachine.java
@@ -22,10 +22,11 @@ public class TicketMachine
      * Create a machine that issues tickets of the given price.
      * Note that the price must be greater than zero, and there
      * are no checks to ensure this.
+     * El precio ahora siempre sera de 800
      */
-    public TicketMachine(int cost)
+    public TicketMachine()
     {
-        price = cost;
+        price = 800;
         balance = 0;
         total = 0;
     }
